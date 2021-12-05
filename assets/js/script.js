4746df3b258394ddf911105b886c3f988819186b
CharReek <charlottereek@outlook.com>
2021-12-05 18:34:17 +0000

    add event listener to start button and test

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+let startButton = document.getElementById("start-button")
+startButton.addEventListener("click" , startGame)
+
+function startGame(){
+console.log("start")
+}
+
+function setQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
