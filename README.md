446865da2368898aa69394532f84cb72ddd2ef57
CharReek <charlottereek@outlook.com>
2021-12-06 21:05:21 +0000

    add function to the question selector and test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
 
 reference 
-https://www.youtube.com/watch?v=riDzcEQbX6k 
+https://www.youtube.com/watch?v=riDzcEQbX6k - how to set up mutiple and make them random questions 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,69 @@
 
 let startButton = document.getElementById("start-button")
 let questionSection = document.getElementById("question-section")
+let questionElement= document.getElementById("question")
+let answerButtons =document.getElementById("answer-section")
+
+let shuffleQuestions, currentQuestion
+
 startButton.addEventListener("click" , startGame)
 
 function startGame(){
 console.log("start")
 startButton.classList.add('hide')
+shuffleQuestions = questions.sort(() => Math.random() -.5)
+currentQuestion = 0
 questionSection.classList.remove('hide')
+setQuestion()
 }
 
 function setQuestion(){
-
+    resetState()
+    showNextQuestion(shuffleQuestions[currentQuestion])
 }
 
-function selectAnswer(){
-
+function showNextQuestion(question){
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        let button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener ('click', selectAnswer)
+        answerButtons.appendChild(button)
+    })
 }
+
+function resetState (){
+    while (answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+
+function selectAnswer(e){
+    let selectedButton = e.target
+    let correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtons.children).forEach(button =>{
+        setStatusClass(button, button.dataset.correct)
+    })
+}
+
+function setStatusClass(element, correct){
+}
+
+
+let questions = [
+    {
+        question: "What does D&G stand for?",
+        answers:[
+            {text: "Dolce & Gabanna", correct: true},
+            {text: "Designer & Garments", correct : false},
+            {text: "Dogs & Goats", correct: false},
+            {text: "Dave & Gary", correct: false}
+        ]
+    }
+]
