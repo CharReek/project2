abf8d793fae47b69771f263f4a740590548f8400
CharReek <charlottereek@outlook.com>
2021-12-06 20:14:24 +0000

    add function to start button to hide on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 
 let startButton = document.getElementById("start-button")
+let questionSection = document.getElementById("question-section")
 startButton.addEventListener("click" , startGame)
 
 function startGame(){
 console.log("start")
+startButton.classList.add('hide')
+questionSection.classList.remove('hide')
 }
 
 function setQuestion(){
