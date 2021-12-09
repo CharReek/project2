4a2f4830e238972a5138cf162c810209323d381b
CharReek <charlottereek@outlook.com>
2021-12-09 12:28:35 +0000

    add event listeners for start and next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
+// vars for the quiz
 const startButton = document.getElementById("start-button");
 const nextButton = document.getElementById("next-button");
 const questionSection = document.getElementById("question-section");
 const questionText = document.getElementById("question-text");
 const answerButtons = document.getElementById("answer-section");
+
+// vars for the questions
+
+let currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
